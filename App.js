12a56491fc54e05b221517f90e7dc5bbf777bcea
Adhiraj Gupta <adhirajgupta2007@gmail.com>
2022-04-30 19:35:25 +0530

    stage1

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Typography} from '@mui/material'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Typography>Hello this is material ui</Typography>
       <StatusBar style="auto" />
     </View>
   );
